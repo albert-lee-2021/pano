3f9236e6497a49157aaef81eb99a36a6247021a0
Albert Lee <albert.lee@alumni.stevens.edu>
2022-04-30 14:57:01 -0400

    fix duplicate panos

--- src/App.js
+++ src/App.js
@@ -11,37 +11,37 @@ import { useState, useEffect } from "react";
 function App() {
   const [entered, setEntered] = useState(false);
   const [panoLoaded, setPanoLoaded] = useState(false);
-  useEffect(() => {
-  const entrance = new PANOLENS.ImagePanorama("Entrancedownscale.jpg");
-  const stairs = new PANOLENS.ImagePanorama("stairs.jpg");
-    // let infospot = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
+  // useEffect(() => {
+  // const entrance = new PANOLENS.ImagePanorama("Entrancedownscale.jpg");
+  // const stairs = new PANOLENS.ImagePanorama("stairs.jpg");
+  //   // let infospot = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
 
-    // infospot.position.set(-100, -500, -5000);
-    // infospot.addHoverText("Stairs");
-    // infospot.addEventListener('click', function () {
-    //   viewer.setPanorama(stairs);
-    // });
-    // entrance.add(infospot);
+  //   // infospot.position.set(-100, -500, -5000);
+  //   // infospot.addHoverText("Stairs");
+  //   // infospot.addEventListener('click', function () {
+  //   //   viewer.setPanorama(stairs);
+  //   // });
+  //   // entrance.add(infospot);
 
-    //Link panos
-  entrance.link(stairs, new THREE.Vector3(4653.43, -1451.79, -1082.51));
+  //   //Link panos
+  // entrance.link(stairs, new THREE.Vector3(4653.43, -1451.79, -1082.51));
 
-  stairs.link(entrance, new THREE.Vector3(3725.45, -3094.47, 1225.24));
+  // stairs.link(entrance, new THREE.Vector3(3725.45, -3094.47, 1225.24));
 
-  //placeholder going upstairs
-  stairs.link(entrance, new THREE.Vector3(4954.39, 531.99, -260.74));
+  // //placeholder going upstairs
+  // stairs.link(entrance, new THREE.Vector3(4954.39, 531.99, -260.74));
 
-  const viewer = new PANOLENS.Viewer({
-    container: document.querySelector("#container"),
-    cameraFov: 80,
-    autoRotate: true,
-    autoRotateSpeed: .5,
-    autoRotateActivationDuration: 10000,
-    autoHideInfospot: false,
-    output: 'console',
-  });
-  viewer.add(entrance, stairs);
-  }, []);
+  // const viewer = new PANOLENS.Viewer({
+  //   container: document.querySelector("#container"),
+  //   cameraFov: 80,
+  //   autoRotate: true,
+  //   autoRotateSpeed: .5,
+  //   autoRotateActivationDuration: 10000,
+  //   autoHideInfospot: false,
+  //   output: 'console',
+  // });
+  // viewer.add(entrance, stairs);
+  // }, []);
 
   return (
     <>
