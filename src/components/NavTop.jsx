7457d0e5b202d3b8c120ca066c2aa919ea8d7975
Diamond <deimantasmazelis@gmail.com>
2022-05-03 17:57:51 +0300

    add new pano

--- src/components/NavTop.jsx
+++ src/components/NavTop.jsx
@@ -1,6 +1,6 @@
 const NavTop = ({ setEntered }) => {
   // let floors = ['Entrance', 'Floor2', 'Loftas', 'Floor 4', 'Floor F'];
-  let floors = ['E', 'F2', 'L', 'F4', 'FF'];
+  let floors = ['E', 'L', 'F4', 'FF'];
   return (
     <div id="nav-top">
       <img style={{ height: '5rem', opcaity: 1 }} src="banner.PNG" alt="" />
--- src/components/Pano.jsx
+++ src/components/Pano.jsx
@@ -2,6 +2,7 @@ import * as PANOLENS from "panolens";
 import * as THREE from 'three';
 import entrance2 from './../assets/entrance.jpg';
 import stairs2 from './../assets/stairs.jpg';
+import koridorius4ds from './../assets/koridorius4ds.jpg';
 import { useEffect } from 'react';
 
 const Pano = () => {
@@ -9,6 +10,7 @@ const Pano = () => {
   useEffect(() => {
     const entrance = new PANOLENS.ImagePanorama(entrance2);
     const stairs = new PANOLENS.ImagePanorama(stairs2);
+    const koridorius4 = new PANOLENS.ImagePanorama(koridorius4ds);
     // let infospot = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
 
     // infospot.position.set(-100, -500, -5000);
@@ -21,6 +23,8 @@ const Pano = () => {
     //Link panos
     entrance.link(stairs, new THREE.Vector3(4653.43, -1451.79, -1082.51));
 
+    entrance.link(koridorius4, new THREE.Vector3(-4798.75, -557.63, -1247.87));
+
     stairs.link(entrance, new THREE.Vector3(3725.45, -3094.47, 1225.24));
 
     //placeholder going upstairs
@@ -29,14 +33,14 @@ const Pano = () => {
     const viewer = new PANOLENS.Viewer({
       container: document.querySelector("#container"),
       cameraFov: 80,
-      autoRotate: true,
+      autoRotate: false,
       autoRotateSpeed: .5,
       autoRotateActivationDuration: 10000,
       autoHideInfospot: false,
-      // output: 'console',
+      output: 'console' 
     });
 
-    viewer.add(entrance, stairs);
+    viewer.add(entrance, stairs, koridorius4);
   }, []);
 
   return (
