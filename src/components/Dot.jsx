2386040cc28959cfea877ef80dd0b7103fbfa4ac
Albert Lee <albert.lee@alumni.stevens.edu>
2022-05-11 07:53:55 -0400

    minor cleanup

--- src/components/Dot.jsx
+++ src/components/Dot.jsx
@@ -1,12 +1,12 @@
 const Dot = ({ dots, setDots, viewer, allDots, dotMatch, i }) => {
     let handleClick = (e) => {
+        e.preventDefault();
         let newDot = {
             i: {
                 ...dots[i], clicked: !dots[i].clicked
             }
         }
         setDots({...dots, ...newDot})
-        e.preventDefault();
         viewer.current.setPanorama(allDots[dotMatch][i].destination);
     }
 
