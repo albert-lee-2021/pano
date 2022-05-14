ea8653c43362f96b8aec7002abc7150518f57c9f
Albert Lee <albert.lee@alumni.stevens.edu>
2022-05-14 10:44:44 -0400

    move top nav right a bit

--- src/components/PlanButtonContainer.jsx
+++ src/components/PlanButtonContainer.jsx
@@ -5,7 +5,7 @@ import PlanImage from './PlanImage';
 const PlanButtonContainer = ({ setEntered, viewer, allDots, floors, plan, setPlan, open, setOpen }) => {
     const [anyClicked, setAnyClicked] = useState('false');
     return (
-        <div style={{ margin: '1rem', display: 'flex', alignItems: 'center', justifyContent: 'start', width: '100vw' }}>
+        <div style={{ margin: '1rem 4rem', display: 'flex', alignItems: 'center', justifyContent: 'start', width: '100vw' }}>
             {Object.keys(floors).map((name, i) =>
                 <PlanButton anyClicked={anyClicked} setAnyClicked={setAnyClicked} floors={floors} plan={plan} setPlan={setPlan} open={open} setOpen={setOpen} name={name} key={name + i}>
                     {name}
